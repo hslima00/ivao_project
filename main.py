07f7ca4b63eedec9b82aac37cced7191d3cf755f
hslima00 <hslima00@gmail.com>
2020-05-17 06:20:02 +0100

    idk

--- main.py
+++ main.py
@@ -3,47 +3,26 @@ from flask_table import Table, Col
 import csv
 import array
 
-
+csv_name = 'teste.csv'
 app = Flask(__name__)
 
 
 @app.route("/ivao")
 def ivao():
 
-    user_id = []
-    nome = []
-    curso = []
-    temp = []
-    items = []
+    item_list = []
 
-    class ItemTable(Table):
-        user_id = Col('ID')
-        nome = Col('NOME')
-        curso = Col('CURSO')
-        # estado = Col('ESTADO')
-
-    class Item(object):
-        def __init__(self, user_id, nome, curso):
-            self.user_id = user_id
-            self.curso = curso
-            self.nome = nome
-
-    with open(r'teste.csv') as csv_file:
+    with open(csv_name) as csv_file:
         read_csv = csv.reader(csv_file)
-        line_index = 0
         for line in read_csv:
 
-            user_id.append(line[0])
-            nome.append(line[1])
-            curso.append(line[2])
-            line_index += 1
+            item = {"user_id": line[0],
+                    "nome": line[1],
+                    "curso": line[2],
+                    "estado": line[3]}  # 0=0ff 1=On}
+            item_list.append(item)
 
-    for x in range(line_index):
-        items.append(Item(user_id[x], nome[x], curso[x]))
-
-    table = ItemTable(items)
-
-    return table.__html__()
+    return render_template('home.html', item_list=item_list)
 
 
 if __name__ == "__main__":
