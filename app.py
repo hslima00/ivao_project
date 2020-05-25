eab03db5894bb94468f25137411193aa651b2c48
hslima00 <hslima00@gmail.com>
2020-05-25 02:54:26 +0100

    csv file opens when running app.py

--- app.py
+++ app.py
@@ -3,53 +3,63 @@
 import configparser
 import csv
 import os
+import threading
 
 from flask import Flask, render_template
 from webeye import get_online_friends, LoginInvalidError
 
+'''GLOBAL VARS'''
 csv_name = 'friends.csv'
+'''-----------------'''
+
 app = Flask(__name__)
 
+'''READING CONFIG.INI'''
 config = configparser.ConfigParser()
 try:
     assert config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 except AssertionError:
     raise FileNotFoundError('config.ini NOT found! Please create one as specified in the README!')
+'''-----------------'''
 
+# Se o login do webeye estiver inválido, este bloco tenta durante 5 segundos até dar timeout
+# todo: implementar async para não bloquear a execução
+# Corre o webeye de x em x tempo, tempo definido pela global var webeye_rerun_time
+# ------------------------------------------------------------------------------------------------------------------
+online_friends = []
+
+def run_webeye():	
+		threading.Timer(100.0, run_webeye).start() #corre a função de x em x tempo x=run_time
+		try:
+				online_friends = get_online_friends(config['ACCOUNT']['user'], config['ACCOUNT']['password'])
+				login_status = 'WebEye login successful'
+		except LoginInvalidError:
+				login_status = 'WebEye login invalid'
+
+		'''ABRIR CSV FILE'''
+		user_list = []
+		try:
+				# todo: this file should be tested on startup
+				with open(csv_name) as csv_file:
+						read_csv = csv.reader(csv_file)
+						for line in read_csv:
+								user = {"user_id": line[0],
+												"nome": line[1],
+												"curso": line[2],
+												# simple hack to check if IDs on friends.csv appear on the online_friends list
+												"estado": 1 if line[0] in ' '.join([user for person in online_friends for user in person]) else 0}  # 0=0ff 1=On}
+								user_list.append(user)
+		except FileNotFoundError:
+				raise FileNotFoundError('friends.csv NOT found! Please create one as specified in the README!')
+		'''-----------------'''
+		return user_list, login_status
+
+users, webeye_login_status = run_webeye()
+print(users)
 
 @app.route("/")
-def ivao():
-
-    online_friends = []
-    # Se o login do webeye estiver inválido, este bloco tenta durante 5 segundos até dar timeout
-    # todo: implementar async para não bloquear a execução
-    # ------------------------------------------------------------------------------------------------------------------
-    try:
-        online_friends = get_online_friends(config['ACCOUNT']['user'], config['ACCOUNT']['password'])
-        login_status = 'WebEye login successful'
-    except LoginInvalidError:
-        login_status = 'WebEye login invalid'
-
-    # ------------------------------------------------------------------------------------------------------------------
-
-    item_list = []
-
-    try:
-        # todo: this file should be tested on startup
-        with open(csv_name) as csv_file:
-            read_csv = csv.reader(csv_file)
-            for line in read_csv:
-
-                item = {"user_id": line[0],
-                        "nome": line[1],
-                        "curso": line[2],
-                        # simple hack to check if IDs on friends.csv appear on the online_friends list
-                        "estado": 1 if line[0] in ' '.join([item for person in online_friends for item in person]) else 0}  # 0=0ff 1=On}
-                item_list.append(item)
-    except FileNotFoundError:
-        raise FileNotFoundError('friends.csv NOT found! Please create one as specified in the README!')
-
-    return render_template('home.html', item_list=item_list, webeye_login_status=login_status)
+def ivao(users, webeye_login_status):		
+		return render_template('home.html', users=users, webeye_login_status=webeye_login_status)
 
 
 if __name__ == "__main__":
